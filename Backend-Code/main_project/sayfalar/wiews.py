19db4b4ffac082841880a48449090b2ca08d053a
Erkut Elik <erkutelik@gmail.com>
2024-01-28 00:25:21 +0300

    projeye anasayfa,iletisim,hakkimizda sayfaları eklendi. Layout klasöründe birleştirildi.

--- Backend-Code/main_project/sayfalar/wiews.py
+++ Backend-Code/main_project/sayfalar/wiews.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def anasayfa(request):
-    return HttpResponse("Anasayfa")
-
+    return render(request,'pages/anasayfa.html')
 
 def iletisim(request):
-    return HttpResponse('iletisim')
+    return render (request,'pages/iletisim.html')
 
 def hakkimizda(request):
-    return HttpResponse("hakkimizda")
+    return render(request,"pages/hakkimizda.html")
