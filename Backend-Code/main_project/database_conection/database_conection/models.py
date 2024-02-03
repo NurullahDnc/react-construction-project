6d334937a9c828a8877414bc492d44d0741620a9
Erkut Elik <erkutelik@gmail.com>
2024-02-04 02:28:14 +0300

    Sql Veritabanı silindi

--- Backend-Code/main_project/database_conection/database_conection/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class sql_connection(models.Model):
-    title=models.CharField(max_length=50)
-    description=models.CharField(max_length=50)
-    imageUrl=models.TextField()
-    date=models.DateField()
-    isActive=models.BooleanField()
--- Backend-Code/main_project/main_project/settings.py
+++ Backend-Code/main_project/main_project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sayfalar',
-    'database_conection'
 ]
 
 MIDDLEWARE = [
