47057246952dd7f7019a74959dfe795e88801ebf
Erkut Elik <erkutelik@gmail.com>
2024-02-11 21:12:42 +0300

    kategoriler eklendi

--- Backend-Code/main_project/sayfalar/admin.py
+++ Backend-Code/main_project/sayfalar/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-from .models import Cars
-admin.site.register(Cars)
+from .models import Cars,categories
+
+
+@admin.register(Cars)
+class Home_Admin(admin.ModelAdmin):
+    list_display = ("title", "isActive", "slug", "Home_Categori")
+    list_display_links = ("title", "slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("title", "isActive", "Home_Categori")
+    list_editable = ("isActive",)
+    search_fields = ("title", "description")
+
+@admin.register(categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display=("name","slug")
+    prepopulated_fields={"slug":("name",),}
