c79609ff46065b9790f2eaca7c8d699e78cb3792
Erkut Elik <erkutelik@gmail.com>
2024-02-19 00:23:17 +0300

    url kısımları değiştirildi, kategoriler ve projeler react kısmına gönderildi.

--- Backend-Code/main_project/sayfalar/urls.py
+++ Backend-Code/main_project/sayfalar/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import anasayfa, hakkimizda, iletisim, details
+from .views import anasayfa, hakkimizda, project, details,categories_
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.anasayfa),
     # path('anasayfa', views.anasayfa),
     path('hakkimizda', views.hakkimizda),
-    path('iletisim', views.iletisim),
+    path('project', views.project_),
+    path('categories', views.categories_),
     path('kategori/<slug>', views.details),
     path('anasayfa', views.anasayfa, name='mymodel-list'),
 ]
--- Backend-Code/main_project/sayfalar/views.py
+++ Backend-Code/main_project/sayfalar/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
-from .models import project, Item
+from .models import categories, project, Item
 from .serializers import ItemSerializer
 from rest_framework import generics
+from . import models
 
 def anasayfa(request):
     kurslar = project.objects.filter(isActive=1).values('title')
@@ -12,9 +13,14 @@ def anasayfa(request):
         'kategoiler': kategoriler
     })
 
-def iletisim(request):
+def project_(request):
     cars = project.objects.all()
-    car_list = [{"id": car.id, "title": car.title, "text": car.text,"slug":car.slug,"model2": car.date,"model2": car.date} for car in cars]
+    car_list = [{"id": car.id, "title": car.title, "text": car.text,"slug":car.slug,"model2": car.date,"model2": car.date,"categories":car.category_id} for car in cars]
+    return JsonResponse({"cars": car_list})
+
+def categories_(request):
+    db = categories.objects.all()
+    car_list = [{"id": db_.id, "title": db_.name, "sulug": db_.slug} for db_ in db]
     return JsonResponse({"cars": car_list})
 
 
